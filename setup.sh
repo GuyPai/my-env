abfccbdc035d228b9547eaace750b69bd6e4b651
Guy <guy.painsky@gmail.com>
2026-03-29 09:57:29 +0300

    add logiops

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-sudo pacman -S sof-firmware vim libreoffice-fresh rofi
+#! /bin/bash
+# sudo pacman -S sof-firmware vim libreoffice-fresh rofi
+
+sudo pacman -S logiops
 mv ~/.bashrc ~/.bashrc.old
 mv ~/.vimrc ~/.vimrc.old
 ln -s ~/my-env/.bashrc ~/.bashrc
 ln -s ~/my-env/.vimrc ~/.vimrc
+ln -s ~/my-env/logid /etc/logid
 source ~/.bashrc
